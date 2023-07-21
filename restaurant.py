edc6054ade9ed05d279c340f07fb267f4802b82f
root <qbitshero@126.com>
2023-07-21 12:44:27 +0800

    Finish restaurant.

--- restaurant.py
+++ restaurant.py
@@ -15,43 +15,55 @@ query = ''' --query https://vm.aleo.org/api '''
 broadcast = '''--broadcast https://vm.aleo.org/api/testnet3/transaction/broadcast '''
 fee_prefix = '''--fee 30000 --record '''
 
-logined_addr_list = []
+consumer_address = ""
 view_key = ""
 private_key = ""
 fee_record_list = []
 
 token_list = []
-cert_list = []
 
 selected_category = ''
 selected_department = ''
 
 def load_config():
-    global logined_addr_list, view_key, private_key, fee_record_list
+    global consumer_address, view_key, private_key, fee_record_list, token_list
 
     fd = open("restaurant.conf", "r+")
-    aleo_address = fd.readline().strip()
-    logined_addr_list.append(aleo_address)
+    consumer_address = fd.readline().strip()
     private_key = fd.readline().strip()
     view_key = fd.readline().strip()
-    fee_record = fd.read().strip()
+
+    records = fd.read().strip()
+    end = records.find('\"', 1)
+    fee_record = records[:end+1].strip()
     fee_record_list.append(fee_record)
+
+    start = records.find('\"', end + 1)
+    end = records.find('\"', start + 1)
+    token = records[start:end+1].strip() 
+    token_list.append(token)
     fd.close()
 
-    logging.info(logined_addr_list[-1])
+    if len(fee_record_list) == 0 or len(token_list) == 0:
+        raise Exception("No token or fee record in restaurant.conf")
+
+    logging.info(consumer_address)
     logging.info(private_key)
     logging.info(view_key)
     logging.info(fee_record_list[-1])
+    logging.info(token_list[-1])
 
 def save_config():
     fd = open("restaurant.conf", "w+")
-    fd.write(logined_addr_list[-1].strip())
+    fd.write(consumer_address.strip())
     fd.write('\n')
     fd.write(private_key.strip())
     fd.write('\n')
     fd.write(view_key.strip())
     fd.write('\n')
     fd.write(fee_record_list[-1])
+    fd.write('\n')
+    fd.write(token_list[-1])
 
     fd.close()
 
@@ -130,6 +142,7 @@ def pay():
     amount = request.form['amount'].strip()
     company = request.form['company'].strip()
     approver1 = request.form['approver'].strip()
+    provider = 'aleo18pfln645uyxsg5x7qq6pc4wca8gegkfgk83j9e89vllvlr8cccpq97l6sj'
 
     logging.info('date = %s, consumer = %s' % (date, consumer))
     logging.info('amount = %s, category = %s, company = %s' % (amount, selected_category, company))
@@ -145,8 +158,8 @@ def pay():
         return render_template('restaurant.html', error = error)
 
     cmd = exe_cmd_prefix + aleo_program + 'transfer_private_with_receipt '
-    input_params = '\"%s\" %s %s ' % (token, privider, amount)
-    receipt_info = '\"{category: %su32, date: %su32, company: %s, dep: %u32, approver: %s}\"' % (selected_category, date, company, selected_department, approver1)
+    input_params = '%s %s %su64 ' % (token_list[-1], provider, amount)
+    receipt_info = '\"{category: %su32, date: %su32, company: %s, dep: %su32, approver: %s}\"' % (selected_category, date, company, selected_department, approver1)
     input_params += receipt_info
     cmd += input_params
     cmd += ''' --private-key ''' + private_key
@@ -174,75 +187,10 @@ def pay():
 
                     cipher_token = trx["execution"]["transitions"][0]["outputs"][0]["value"]
                     plain_token = decrypt_record(cipher_token.strip())
+                    plain_token = '\"' + plain_token + '\"'
                     token_list.append(plain_token)
 
-                    cert = trx["execution"]["transitions"][0]["outputs"][1]["value"]
-                else:
-                    error = "Failed to query transaction. Please check it on block chain. %s" % trxid
-
-    return render_template('restaurant.html', error = error)
-
-# get certificate, which is used to log in server system
-# we transfer login token to server account, and get a certificate
-@app.route('/get_cert', methods=['POST'])
-def get_cert():
-    server_address = request.form['server_address'].strip()
-    logging.info(server_address)
-
-    if len(token_list) == 0:
-        error = "No login token available. Please check config file."
-        return render_template('restaurant.html', error = error)
-
-    logging.info(token_list[-1])
-    token = token_list[-1]
-    index = token.find("issuer")
-    if index < 0:
-        error = "The token is invalid."
-        logging.error(error + " No issuer field")
-        return render_template("restaurant.html", error = error)
-
-    end = token.find(".private", index)
-    if end < 0:
-        error = "The token is invalid."
-        logging.error(error + " Issuer is not private")
-        return render_template("restaurant.html", error = error)
-
-    issuer = token[index + 8:end]
-    if issuer != server_address:
-        error = "No login token for the server. Please check it."
-        return render_template('restaurant.html', error = error)
-
-    cmd = exe_cmd_prefix + aleo_program + 'login '
-    input_params = '\"%s\" %uu64 %uu32 ' % (token, SERVICE1_UNIT, SERVICE1_ID)
-    cmd += input_params
-    cmd += ''' --private-key ''' + private_key
-    cmd += query
-    cmd += broadcast
-    cmd += fee_prefix
-    cmd += fee_record_list[-1]
-
-    logging.info(get_current_height())
-    logging.info(cmd)
-    error = ""
-    try:
-        output = subprocess.check_output(cmd, shell=True)
-        logging.info(output)
-
-        if output.find("Successfully") >= 0:
-            index = output.rfind("at1")
-            if index >= 0:
-                trxid = output[index:].strip()
-                trx = get_transaction(trxid)
-                fee_record = "Error"
-                if trx != "":
-                    fee_record_cipher = trx["fee"]["transition"]["outputs"][0]["value"]
-                    fee_record = decrypt_record(fee_record_cipher)
-
-                    cipher_token = trx["execution"]["transitions"][0]["outputs"][0]["value"]
-                    plain_token = decrypt_record(cipher_token.strip())
-                    token_list.append(plain_token)
-
-                    cert = trx["execution"]["transitions"][0]["outputs"][1]["value"]
+                    receipt = trx["execution"]["transitions"][0]["outputs"][2]["value"]
                 else:
                     error = "Failed to query transaction. Please check it on block chain. %s" % trxid
 
@@ -255,7 +203,8 @@ def get_cert():
                         error = "Failed to decrypt fee record cipher."
                     logging.error(error)
 
-                return render_template('restaurant.html', error = error, cert = cert)
+                logging.info('Receipt: %s' % receipt)
+                return render_template('restaurant.html', error = error, receipt = receipt)
             else:
                 error = "No transaction found in the successful result."
         else:
@@ -268,6 +217,7 @@ def get_cert():
         logging.error(error)
 
     return render_template('restaurant.html', error = error)
+
 
 if __name__ == "__main__":
         logging.getLogger().setLevel(logging.INFO)
